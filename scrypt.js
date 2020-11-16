63608dd989918b8e81085844ee3c0666110c0c64
Aleksandar Ivezic <a.ivezic@yahoo.com>
2020-11-16 18:36:32 +0100

    Cleaning code , add some responsivenes

--- scrypt.js
+++ scrypt.js
@@ -1,48 +1,5 @@
-function add (n1, n2) {
-	
-	return n1 + n2;
-}
 
-function subtract (n1, n2) {
-	return n1-n2;
-}
-
-function sum (ar) {
-	if (ar){
-	return ar.reduce(( accumulator, currentValue ) => accumulator + currentValue , 0);
-	} else {
-	return 0;
-	}
-
-}
-
-function multiply (n1, n2) {
-	return n1 * n2;
-}
-function divide (n1,n2) {
-    return n1/n2;
-}
-
-function power(num, p) {
-	let n = num;
-	for (let i=1; i<p; i++){
-		n = n * num;
-	}
-	return n;
-}
-
-function factorial(n) {		
-	if (n> 0) {
-		let f=n;
-		for  (let x = 1; x<n; x++) {
-			f = f * (n-x);
-		}
-		return f;
-	} else {
-		return 1;
-	}	
-}
-
+// Variables
 
 const button1 = document.getElementById("1").addEventListener("click", populateDisplay);
 const button2 = document.getElementById("2").addEventListener("click", populateDisplay);
@@ -68,40 +25,59 @@ let displayValue1 = "";
 let storedValue = "";
 let storedValue1 = "";
 let operator = "";
+const container= document.getElementById("container");
 
+//Functions
 
+function add (n1, n2) {
+	
+	return n1 + n2;
+}
 
+function subtract (n1, n2) {
+	return n1-n2;
+}
 
+function multiply (n1, n2) {
+	return n1 * n2;
+}
+function divide (n1,n2) {
+    return n1/n2;
+}
+
+function power(num, p) {
+	let n = num;
+	for (let i=1; i<p; i++){
+		n = n * num;
+	}
+	return n;
+}
 
 function populateDisplay(e){
     if(typeof storedValue == "string" ) {
         displayValue = displayValue + e.target.textContent;
         if (displayValue.length > 16) {
+            
             displayValue=displayValue.substring(0,16);
+        }
+        if(window.outerWidth < 500) {
+            displayValue=displayValue.substring(0,11);
         }
         display.textContent= Number(displayValue);
     }else {
         
         displayValue1 = displayValue1 + e.target.textContent;
         if (displayValue1.length > 15) {
+            
             displayValue1=displayValue1.substring(0,15);
+        }
+        if(window.outerWidth < 500) {
+            displayValue1=displayValue1.substring(0,10);
         }
         display.textContent = displayValue1;
         storedValue1 =Number(displayValue1);
-    }   
-    
-  
+    }     
 }
-function updateDisplay() {
-    
-    if(displayValue.length > 12 ) {
-        display.textContent = displayValue.substring(0, 12);
-    }else if (displayValue1.length>12) {
-
-    }
-}
-  
-updateDisplay();
 
 function addOperator(e) {
     console.log(storedValue, storedValue1);
@@ -142,16 +118,3 @@ function operate(opt, num1, num2) {
 function clear(e) {
     location.reload();
 }
-
-/* function equal(e) {
-    console.log(storedValue);
-    console.log(op);
-    console.log(displayValue1);
-    
-    console.log(storedValue);
-    display.textContent = storedValue + Number(displayValue1);
-    displayValue=storedValue;
-} */
-
-//console.log(displayValue);
-//console.log(button1);
